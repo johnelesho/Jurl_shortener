91d98e8c9fdc10e0407292b59f88f025b8fe85c0
John Elesho <eleshojohno@gmail.com>
2023-01-23 22:51:14 +0100

    Initial project set up

---
+++ src/main/java/com/elsoft/assessment/jurlshortener/JUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.elsoft.assessment.jurlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUrlShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JUrlShortenerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elsoft/assessment/jurlshortener/JUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsoft.assessment.jurlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
