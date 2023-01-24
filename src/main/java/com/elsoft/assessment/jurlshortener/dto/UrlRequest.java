7cff2d7ad19c72bc2fd5668dfde741b444c63851
John Elesho <eleshojohno@gmail.com>
2023-01-24 23:47:49 +0100

    Writing Tests

--- src/main/java/com/elsoft/assessment/jurlshortener/dto/UrlRequest.java
+++ src/main/java/com/elsoft/assessment/jurlshortener/dto/UrlRequest.java
@@ -1,5 +1,9 @@
 package com.elsoft.assessment.jurlshortener.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import jakarta.validation.constraints.NotBlank;
@@ -22,7 +26,8 @@ public class UrlRequest {
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @ApiModelProperty(required = false, notes = "Please indicate when you want it to expire")
-
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime expiryDate ;
 
     @ApiModelProperty(required = false, notes = "Please indicate how long (days) you want it to be active")
--- src/main/java/com/elsoft/assessment/jurlshortener/dto/UrlResponse.java
+++ src/main/java/com/elsoft/assessment/jurlshortener/dto/UrlResponse.java
@@ -1,5 +1,9 @@
 package com.elsoft.assessment.jurlshortener.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,9 +21,11 @@ public class UrlResponse implements Serializable {
 
     private String url;
 
-
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createdDate;
-
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime expiryDate;
 
 
